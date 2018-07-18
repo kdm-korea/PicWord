cb10c2a20501da2724b454911ffd92423e85da51
kdm-korea <kdm_korea@naver.com>
2018-07-18 11:20:41 +0900

    Feat : throw stuff / used physics engine

---
+++ Code/Introduction-To-HTC-Vive-Starter/Introduction To HTC Vive - Starter/Assets/Scripts/ControllerGrabObject.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    private GameObject collidingObject;
+    private GameObject objectInHand;
+
+    private SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+	
+    void Awake() {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private void SetColldingObject(Collider col) {
+        if(collidingObject || !col.GetComponent < Rigidbody>()) {
+            return;
+        }
+        collidingObject = col.gameObject;
+    }
+
+    private void GrabObject() {
+        var joint = AddFixedJoint();
+        objectInHand = collidingObject;
+        collidingObject = null;
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    private FixedJoint AddFixedJoint() {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void RelaseObject() {
+        if (GetComponent<FixedJoint>()) {
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        objectInHand = null;
+    }
+
+	void Update () {
+        if (Controller.GetHairTriggerDown()) {
+            if (collidingObject) {
+                GrabObject();
+            }
+        }else if (Controller.GetHairTriggerUp()) {
+            if (objectInHand) {
+                RelaseObject();
+            }
+        }
+	}
+}
