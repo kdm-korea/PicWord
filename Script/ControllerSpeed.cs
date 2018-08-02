cc64cd3daef78da96b3120d875c4b8a7368849d9
kdm-korea <kdm_korea@naver.com>
2018-08-02 16:02:19 +0900

    Post: ControllerSpeed Class

---
+++ Script/ControllerSpeed.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSpeed : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+    Vector3 tmpPos;
+
+    public  SteamVR_Controller.Device Controller {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    private void Start()
+    {
+        trackedObj = GetComponentInParent<SteamVR_TrackedObject>();
+        tmpPos = trackedObj.transform.position;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        tmpPos =- trackedObj.transform.position;
+        Debug.Log(tmpPos = -trackedObj.transform.position);
+    }
+}
