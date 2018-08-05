dca0127f5a9426bc2563dd38cd3f4723cb1bee97
kdm-korea <kdm_korea@naver.com>
2018-08-05 16:03:34 +0900

    Post: Calculator Class

---
+++ Script/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Calculator : MonoBehaviour {
+
+    public float GetX(float degree, float radius)
+    {
+        return Mathf.Cos(degree * Mathf.Deg2Rad) * radius;
+    }
+
+    public float GetY(float degree, float radius)
+    {
+        return Mathf.Sin(degree * Mathf.Deg2Rad) * radius;
+    }
+
+    public Vector3 GetPosition(float degree, float radius, float height)
+    {
+        float x = GetX(degree, radius);
+        float z = GetY(degree, radius);
+
+        return new Vector3(x, height, z);
+    }
+}
