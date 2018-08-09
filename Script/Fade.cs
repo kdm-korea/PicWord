37a9d1d532fd31a35d83287b34973041d2e57e23
kdm-korea <kdm_korea@naver.com>
2018-08-09 16:06:24 +0900

    Post: Fade Class

---
+++ Script/Fade.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade : MonoBehaviour {
+
+    public void FadeOut(GameObject go){
+        go.GetComponent<SteamVR_Fade>();
+        SteamVR_Fade.Start(Color.black,2.0f);
+    }
+    public void FadeIn(GameObject go)
+    {
+        go.GetComponent<SteamVR_Fade>();
+        SteamVR_Fade.Start(Color.clear,2.0f);
+    }
+}
